98abca617feec08c9dfb8c9c2c6e53044ac185d2
dk126 <44493065+dk126@users.noreply.github.com>
2019-11-13 13:07:06 +0100

    13.11

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace nowy_13._11
+{
+    class Program
+    {
+        static void Licz()
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                Console.WriteLine("wątek: " + i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread thr = new Thread(Licz);
+            thr.Start();
+            for (int i = 1; i <= 1000; i++)
+            {
+                Console.WriteLine("program1: " + i);
+            }
+            Console.WriteLine();
+           
+            Console.ReadKey();
+        }
+    }
+}
